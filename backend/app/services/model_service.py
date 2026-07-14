bd84d5d92690c540c3e955d97a489a1ee743a4ae
Elameen Badawy <elameenbadawy@gmail.com>
2026-07-14 14:02:37 +0100

    Update model_service.py

--- backend/app/services/model_service.py
+++ backend/app/services/model_service.py
@@ -71,9 +71,11 @@ def predict(image: Image.Image):
         save_path,
         overlay,
     )
-    return {
-        "prediction": CLASS_NAMES[predicted_index],
-        "confidence": confidence,
-        "probabilities": probabilities,
-        "gradcam": f"/static/gradcam/{filename}",
-    }
+    BACKEND_URL = "https://brain-tumor-mri-classification-k8sx.onrender.com"
+
+return {
+    "prediction": CLASS_NAMES[predicted_index],
+    "confidence": confidence,
+    "probabilities": probabilities,
+    "gradcam": f"{BACKEND_URL}/static/gradcam/{filename}",
+}
